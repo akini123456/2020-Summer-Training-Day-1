01a5fbfe56b7ecca9bdadb9a50f26b56d58b4487
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2020-07-23 12:51:21 -0400

    Aryan's Nickname and Icebreaker

--- Day1/src/main/java/frc/robot/IceBreaker.java
+++ Day1/src/main/java/frc/robot/IceBreaker.java
@@ -5,10 +5,12 @@ public class IceBreaker {
     // NICKNAMES
     // Michael Gorbunov - Michael
     // Siddharth Lohani - Sid
+    //Aryan Kini - Akini
 
     // Embarassing moments
     // Michael Gorbunov - Once asked out someone who turned out to be lesbian.
     // Siddharth Lohani - In first grade I had to go to the ER because I put a rock in my ear and I couldn't get it out.
+    //Aryan Kini- I'm a meme on Cheif Delphi because I posted a message saying Radio too hot and smelling really bad
 
     public IceBreaker () {
 
